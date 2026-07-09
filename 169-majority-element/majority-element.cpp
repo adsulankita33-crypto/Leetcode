cedfba5277e1b7c57bc496dfb1b3fc45db7a8395
adsulankita33-crypto <adsulankita33@gmail.com>
2026-07-09 14:09:12 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (33.00%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count =0;int candiadate=0;
+        for(int i=0;i<nums.size();i++){
+            if(count==0){
+                candiadate=nums[i];
+            }
+            if(candiadate==nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return candiadate;
+    }
+};
