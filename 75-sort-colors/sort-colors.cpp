0e90357afb3a0deaf46307c990ffabf01319ce40
adsulankita33-crypto <adsulankita33@gmail.com>
2026-07-11 19:26:45 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (49.05%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int low=0;int mid=0;int high=nums.size()-1;
+        int mid=0;int low=0;int high=nums.size()-1;
         while(mid<=high){
             if(nums[mid]==0){
                 swap(nums[mid],nums[low]);
-                low++;
                 mid++;
+                low++;
             }
             else if(nums[mid]==1){
                 mid++;
@@ -16,6 +16,5 @@ public:
                 high--;
             }
         }
-       
     }
 };
