fd3bcc4192197f0e4e258dc65bea548200b5dd79
adsulankita33-crypto <adsulankita33@gmail.com>
2026-08-04 20:38:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (43.71%) - LeetSync

---
+++ 59-spiral-matrix-ii/spiral-matrix-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>>matrix(n,vector<int>(n));
+        int startrow=0;int endrow=n-1;
+        int startcoln=0;int endcoln=n-1;
+        int num=1;
+        while(startrow<=endrow && startcoln<=endcoln){
+            for(int j=startcoln;j<=endcoln;j++){
+                matrix[startrow][j]=num++;
+            }
+            startrow++;
+            for(int i=startrow;i<=endrow;i++){
+                matrix[i][endcoln]=num++;
+            }
+            endcoln--;
+            if(startrow<=endrow){
+            for(int j=endcoln;j>=startcoln;j--){
+                matrix[endrow][j]=num++;
+            }
+            endrow--;
+            }
+            if(startcoln<=endcoln){
+            for(int i=endrow;i>=startrow;i--){
+                matrix[i][startcoln]=num++;
+            }
+            startcoln++;
+        }
+        }
+        return matrix;
+    }
+};
