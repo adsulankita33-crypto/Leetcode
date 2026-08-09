bf2a08be578b0c3b49b4d3cb22cfc1a0853f0284
adsulankita33-crypto <adsulankita33@gmail.com>
2026-08-09 11:17:56 +0530

    Time: 2 ms (97.07%) | Memory: 13.5 MB (64.48%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int n;
+    vector<int> suffix;
+    vector<vector<int>> memo;
+
+    int solve(int i, int M) {
+
+        if (i >= n)
+            return 0;
+
+        if (i + 2 * M >= n)
+            return suffix[i];
+
+        if (memo[i][M] != -1)
+            return memo[i][M];
+
+        int ans = 0;
+
+        for (int X = 1; X <= 2 * M; X++) {
+
+            ans = max(
+                ans,
+                suffix[i] -
+                solve(i + X, max(M, X))
+            );
+        }
+
+        return memo[i][M] = ans;
+    }
+
+    int stoneGameII(vector<int>& piles) {
+
+        n = piles.size();
+
+        suffix.resize(n);
+
+        suffix[n - 1] = piles[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = piles[i] + suffix[i + 1];
+        }
+
+        memo.assign(n, vector<int>(n + 1, -1));
+
+        return solve(0, 1);
+    }
+};
