7c2b572bfca54f119798a43948137e4fc6e2e12b
adsulankita33-crypto <adsulankita33@gmail.com>
2026-08-14 12:00:22 +0530

    Time: 2555 ms (7.29%) | Memory: 34.1 MB (99.46%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int count=0;
         int n=nums.size();
+        int count=0;
         for(int i=0;i<n;i++){
             int sum=0;
             for(int j=i;j<n;j++){
