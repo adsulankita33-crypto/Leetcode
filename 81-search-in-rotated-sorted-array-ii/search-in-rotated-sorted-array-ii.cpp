e37eb426b8ded3a525a047af08c170528139887a
adsulankita33-crypto <adsulankita33@gmail.com>
2026-08-15 13:40:19 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (46.96%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -8,27 +8,26 @@ public:
             if(nums[mid]==target){
                 return true;
             }
-         if(nums[start]==nums[mid] && nums[mid]==nums[end]){
-            start++;
-            end--;
-         }
-         else if(nums[start]<=nums[mid]){
-            if(nums[start]<=target && target <nums[mid]){
-                end=mid-1;
+            if(nums[start]==nums[mid] && nums[mid]==nums[end]){
+                start++;end--;
+            }
+            else if(nums[start]<=nums[mid]){
+                if(nums[start]<=target && target<nums[mid]){
+                    end=mid-1;
+                }
+                else{
+                    start=mid+1;
+                }
             }
             else{
-                start=mid+1;
+                if(target>nums[mid] && target<=nums[end]){
+                    start=mid+1;
+                }
+                else{
+                    end=mid-1;
+                }
             }
-         }
-         else{
-            if(nums[mid]<target && target<=nums[end]){
-                start=mid+1;
-            }
-            else{
-                end=mid-1;
-            }
-         }
-       }
-       return false;  
+        }
+        return false;
     }
 };
