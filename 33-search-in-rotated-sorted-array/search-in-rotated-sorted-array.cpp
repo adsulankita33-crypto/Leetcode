d1cb8bad8fc82c810db242639d333b738aaea9bd
adsulankita33-crypto <adsulankita33@gmail.com>
2026-08-22 15:13:22 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (36.82%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n=nums.size();
-        int start=0;int end=nums.size()-1;
+        int start=0;
+        int end=nums.size()-1;
         while(start<=end){
             int mid=start+(end-start)/2;
             if(nums[mid]==target){
@@ -17,13 +17,16 @@ public:
                 }
             }
             else{
-                if(nums[mid]<=target && target<=nums[end]){
-                    start=mid+1;
-                }
-                else{
-                    end=mid-1;
+                if(nums[mid]<=nums[end]){
+                    if(nums[mid]<=target && target<=nums[end]){
+                        start=mid+1;
+                    }
+                    else{
+                        end=mid-1;
+                    }
                 }
             }
-        }return -1;
+        }
+        return -1;
     }
 };
